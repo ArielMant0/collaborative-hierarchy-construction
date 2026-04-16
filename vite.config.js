4083c27fdf41c13d9a37390dab7fcadce512da42
ArielMant0 <buecher.apps@gmail.com>
2026-04-16 18:09:24 +0200

    added deploy workflow (untested)
    - removed node_modules
    - added base path to vite config
    - added vite dev/build commands required for deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const BASE_PATH = "/collaborative-hierarchy-construction"
+
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [vue()],
+  server: { base: BASE_PATH },
+  preview: { base: BASE_PATH }
 })
